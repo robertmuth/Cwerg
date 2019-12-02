7432a47e823b6830da2e30370e2edf33e98dd001
Robert Muth <robert@muth.org>
2019-12-01 23:14:09 -0500

    move all of the printf splitter to printf_transform.py

--- canonicalize.py
+++ canonicalize.py
@@ -126,76 +126,6 @@ def AddExplicitCasts(node: c_ast.Node, parent: c_ast.Node, meta_info, skip_const
 
 
 # ================================================================================
-# This transformation splits printf calls with constant format string into several printf
-# calls. Each of which has at most one argument besides the format string
-# This eliminates the primary use case of variable argument lists.
-#
-# This invalidates sym_tab and type_tab
-# ================================================================================
-def IsSuitablePrintf(node: c_ast.Node, _):
-    if not isinstance(node, c_ast.FuncCall): return None
-    if not isinstance(node.name, c_ast.ID): return None
-    if node.name.name != "printf": return None
-    assert isinstance(node.args, c_ast.ExprList)
-    assert len(node.args.exprs) > 0
-    format_arg = node.args.exprs[0]
-    if not isinstance(format_arg, c_ast.Constant): return None
-    assert format_arg.type == "string"
-    return True
-
-
-# TODO: update sym and type tabs
-def GetStatementList(node: c_ast.Node):
-    if isinstance(node, (c_ast.Default, c_ast.Case)):
-        return node.stmts
-    elif isinstance(node, c_ast.Compound):
-        return node.block_items
-    else:
-        # return None
-        assert False, node
-
-
-def MakePrintfCall(fmt_str, arg_node: Optional[c_ast.Node]):
-    args = [c_ast.Constant("string", '"' + fmt_str + '"')]
-    if arg_node: args.append(arg_node)
-    return c_ast.FuncCall(c_ast.ID("printf"), c_ast.ExprList(args))
-
-
-def DoPrintfSplitter(call: c_ast.FuncCall, parent, meta_info: meta.MetaInfo):
-    args = call.args.exprs
-    fmt_pieces = printf_transform.TokenizeFormatString(args[0].value[1:-1])
-    if not fmt_pieces: return
-    if len(fmt_pieces) == 1: return
-
-    stmts = GetStatementList(parent)
-    if not stmts:
-        assert False, parent
-
-    calls = []
-    args = args[1:]  # skip the format string
-    for f in fmt_pieces:
-        arg = None
-        if f[0] == '%' and len(f) > 1:
-            arg = args.pop(0)
-        c = MakePrintfCall(f, arg)
-        meta_info.type_links[c] = meta_info.type_links[call]
-        meta_info.type_links[c.args] = meta_info.type_links[call.args]
-        meta_info.type_links[c.args.exprs[0]] = meta_info.type_links[call.args.exprs[0]]
-        meta_info.type_links[c.name] = meta_info.type_links[call.name]
-        meta_info.sym_links[c.name] = meta_info.sym_links[call.name]
-        calls.append(c)
-    pos = stmts.index(call)
-    stmts[pos:pos + 1] = calls
-
-
-def PrintfSplitter(ast: c_ast.Node, meta: meta.MetaInfo):
-    candidates = common.FindMatchingNodesPostOrder(ast, ast, IsSuitablePrintf)
-
-    for call, parent in candidates:
-        DoPrintfSplitter(call, parent, meta)
-
-
-# ================================================================================
 # Transform post inc/dev to pre inc/dec which are sementically simpler
 # ================================================================================
 def IsSuitablePostIncDec(node, parent):
@@ -454,7 +384,7 @@ def Canonicalize(ast: c_ast.Node, meta_info: meta.MetaInfo):
     CanonicalizeBaseTypes(ast)
     meta_info.CheckConsistency(ast)
 
-    PrintfSplitter(ast, meta_info)
+    printf_transform.PrintfSplitterTransform(ast, meta_info)
     meta_info.CheckConsistency(ast)
 
     FixNodeRequiringBoolInt(ast, meta_info)
--- printf_transform.py
+++ printf_transform.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
 import re
+from typing import Optional
+
+from pycparser import c_ast
+
+import common
+import meta
+
+__all__ = ["TokenizeFormatString", "FormatOptions"]
 
 PRINTF_OPTION = re.compile(r"%([-+ 0]*)([0-9]*)([.][0-9]+)?([lqh]+)?([cduoxefgsp])")
 
@@ -53,6 +61,76 @@ def TokenizeFormatString(s: str):
         out.append(m.group(0))
     add_plain(last, len(s))
     return out
+
+
+# ================================================================================
+# This transformation splits printf calls with constant format string into several printf
+# calls. Each of which has at most one argument besides the format string
+# This eliminates the primary use case of variable argument lists.
+#
+# This invalidates sym_tab and type_tab
+# ================================================================================
+def _IsSuitablePrintf(node: c_ast.Node, _):
+    if not isinstance(node, c_ast.FuncCall): return None
+    if not isinstance(node.name, c_ast.ID): return None
+    if node.name.name != "printf": return None
+    assert isinstance(node.args, c_ast.ExprList)
+    assert len(node.args.exprs) > 0
+    format_arg = node.args.exprs[0]
+    if not isinstance(format_arg, c_ast.Constant): return None
+    assert format_arg.type == "string"
+    return True
+
+
+# TODO: update sym and type tabs
+def GetStatementList(node: c_ast.Node):
+    if isinstance(node, (c_ast.Default, c_ast.Case)):
+        return node.stmts
+    elif isinstance(node, c_ast.Compound):
+        return node.block_items
+    else:
+        # return None
+        assert False, node
+
+
+def MakePrintfCall(fmt_str, arg_node: Optional[c_ast.Node]):
+    args = [c_ast.Constant("string", '"' + fmt_str + '"')]
+    if arg_node: args.append(arg_node)
+    return c_ast.FuncCall(c_ast.ID("printf"), c_ast.ExprList(args))
+
+
+def _DoPrintfSplitter(call: c_ast.FuncCall, parent, meta_info: meta.MetaInfo):
+    args = call.args.exprs
+    fmt_pieces = TokenizeFormatString(args[0].value[1:-1])
+    if not fmt_pieces: return
+    if len(fmt_pieces) == 1: return
+
+    stmts = GetStatementList(parent)
+    if not stmts:
+        assert False, parent
+
+    calls = []
+    args = args[1:]  # skip the format string
+    for f in fmt_pieces:
+        arg = None
+        if f[0] == '%' and len(f) > 1:
+            arg = args.pop(0)
+        c = MakePrintfCall(f, arg)
+        meta_info.type_links[c] = meta_info.type_links[call]
+        meta_info.type_links[c.args] = meta_info.type_links[call.args]
+        meta_info.type_links[c.args.exprs[0]] = meta_info.type_links[call.args.exprs[0]]
+        meta_info.type_links[c.name] = meta_info.type_links[call.name]
+        meta_info.sym_links[c.name] = meta_info.sym_links[call.name]
+        calls.append(c)
+    pos = stmts.index(call)
+    stmts[pos:pos + 1] = calls
+
+
+def PrintfSplitterTransform(ast: c_ast.Node, meta: meta.MetaInfo):
+    candidates = common.FindMatchingNodesPostOrder(ast, ast, _IsSuitablePrintf)
+
+    for call, parent in candidates:
+        _DoPrintfSplitter(call, parent, meta)
 
 
 if __name__ == "__main__":
