d9d913ed54d9755083073a37eacda5c1156d1cac
Robert Muth <robert@muth.org>
2020-01-31 22:14:12 -0500

    tweak test

--- Tests/2003-07-08-BitOpsTest.c
+++ Tests/2003-07-08-BitOpsTest.c
@@ -1,17 +1,19 @@
 int printf( const char *restrict format, ... );
 
 
-void test(int A, int B, int C, int D) {
-  int bxor = A ^ B ^ C ^ D;
-  int bor  = A | B | C | D;
-  int band = A & B & C & D;
-  int bandnot = (A & ~B) ^ (C & ~D);
-  int bornot  = (A | ~B) ^ (C | ~D);
+void test(unsigned int A, unsigned int B, unsigned int C, unsigned int D) {
+  printf("%x %x %x %x\n", A, B, C, D);
+  unsigned int bxor, bor, band, bandnot, bornot;
+  bxor = A ^ B ^ C ^ D;
+  bor  = A | B | C | D;
+  band = A & B & C & D;
+  bandnot = (A & ~B) ^ (C & ~D);
+  bornot  = (A | ~B) ^ (C | ~D);
   
-  printf("%d %d %d %d %d\n", bxor, bor, band, bandnot, bornot);
+  printf("%x %x %x %x %x\n", bxor, bor, band, bandnot, bornot);
 }
 
 int main() {
-  test(7, 8, -5, 5);
+  test(7, 8, (unsigned int)-5, 5);
   return 0;
 }
