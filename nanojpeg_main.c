558da8d1f303d5998e7266a6eda86aa54887b326
Robert Muth <robert@muth.org>
2020-02-14 16:22:18 -0500

    remove heade deps for nanojpeg test

--- nanojpeg_main.c
+++ nanojpeg_main.c
@@ -2,9 +2,40 @@
 
 #include "nanojpeg.c"
 
-#include <stdio.h>
+int printf( const char *restrict format, ... );
+void* malloc(unsigned long size);
+char*  itoa (int value, char* str, int base);
 
 
+
+#define FILE void
+
+FILE* fopen(const char *pathname, const char *mode);
+int fclose(FILE* stream);
+int fseek(FILE* stream, long offset, int whence);
+long ftell(FILE* stream);
+
+unsigned long fread(void *ptr, unsigned long size, unsigned long nmemb, FILE* stream);
+unsigned long fwrite(void *ptr, unsigned long size, unsigned long nmemb, FILE* stream);
+int fputs(const char *restrict s, FILE *stream);
+
+const int SEEK_END = 2;
+const int SEEK_SET = 0;
+
+void fwrite_d(FILE* f, int a) {
+  char buf[64];
+  int i = 63;
+  buf[i] = 0;
+  --i;
+
+  // assume only pos a for now
+  do {
+        buf[i] = '0' + a % 10;
+        --i;
+        a /= 10;
+  } while (a != 0);
+  fputs(&buf[i+1], f);
+}
 
 int main(int argc, char* argv[]) {
     int size;
@@ -40,7 +71,13 @@ int main(int argc, char* argv[]) {
         printf("Error opening the output file.\n");
         return 1;
     }
-    fprintf(f, "P%d\n%d %d\n255\n", njIsColor() ? 6 : 5, njGetWidth(), njGetHeight());
+    fputs(njIsColor() ? "P6\n" : "P5\n", f);
+    fwrite_d(f, njGetWidth());
+    fputs(" ", f);
+    fwrite_d(f, njGetHeight());
+    fputs("\n", f);
+    fputs("255\n", f);
+
     fwrite(njGetImage(), 1, njGetImageSize(), f);
     fclose(f);
     njDone();
