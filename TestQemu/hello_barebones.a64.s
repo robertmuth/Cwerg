be120e005d8a176e70e8d00b376f3f71e262f0b4
Robert Muth <robert@muth.org>
2021-05-21 11:43:22 -0400

    fix qemu a64 stuff

--- TestQemu/hello_barebones.a64.s
+++ TestQemu/hello_barebones.a64.s
@@ -5,7 +5,7 @@ _start:
 	mov x0, #1 	// stdout
 	adrp x1, _msg
 	add x1, x1, :lo12:_msg
-	mov x2, #(_msg_end - _msg)
+	mov x2, #(_msg_end - _msg - 1)
 	mov x8, #64  	// 'write' syscall
 	svc #0                  
 
@@ -17,13 +17,13 @@ _msg:
       .asciz "Hello world (asm)\n"
 _msg_end:	
 
-	.section ".bss"
-	.byte 0, 0, 0, 0
+//	.section ".bss"
+//	.byte 0, 0, 0, 0
 	
-	.section ".data"
-	.byte 1, 2, 3, 4
+//	.section ".data"
+//	.byte 1, 2, 3, 4
 
 
 
-	.section ".rodata"
-	.byte 0, 0, 0, 0
+//	.section ".rodata"
+//	.byte 0, 0, 0, 0
