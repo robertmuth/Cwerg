b81c9c2893e1646e5e7b4668094176b7d03a50e0
Robert Muth <robert@muth.org>
2021-06-04 11:23:13 -0400

    add sieve example to docs

--- FrontEndC/TestData/sieve.c
+++ FrontEndC/TestData/sieve.c
@@ -1,5 +1,5 @@
 
- #include "std_lib.h"   // needed because printf may be rewritten to call helpers defined here
+#include "std_lib.h"   // needed because printf may be rewritten to call helpers defined here
   
   
 int printf( const char *restrict format, ... );
@@ -9,13 +9,13 @@ int printf( const char *restrict format, ... );
 
 char is_prime[SIZE];
 
-unsigned sieve (int repeats) {
+unsigned sieve (unsigned repeats) {
   unsigned num_primes; 
 
   for (unsigned n = 0; n < repeats; n++) {
     num_primes = 0;
-    for (int i = 0; i < SIZE; i++) is_prime[i] = 1;
-    for (int i = 0; i < SIZE; i++)
+    for (unsigned i = 0; i < SIZE; i++) is_prime[i] = 1;
+    for (unsigned i = 0; i < SIZE; i++)
       if (is_prime[i]) {
         unsigned prime = i + i + 3;
         for (unsigned k = i + prime; k < SIZE; k += prime) is_prime[k] = 0;
@@ -27,6 +27,6 @@ unsigned sieve (int repeats) {
 
 
 int main() {
-  if (sieve(1) != 148932) abort();
+  if (sieve(1U) != 148932) abort();
   return 0;
 }
