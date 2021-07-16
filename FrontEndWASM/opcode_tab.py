b49902c269327195bf30f9080971c80980574de7
Robert Muth <robert@muth.org>
2021-07-16 10:49:03 -0400

    started work on WASM frontend

---
+++ FrontEndWASM/opcode_tab.py
@@ -0,0 +1,320 @@
+#!/usr/bin/python3
+
+import typing
+import enum
+
+
+###########################################################
+# Opcodes
+# https://webassembly.github.io/spec/core/binary/instructions.html#control-instructions
+############################################################
+
+@enum.unique
+class ARG_TYPE(enum.IntEnum):
+    INVALID = 0
+    UINT = 1
+    BYTE1_ZERO = 2
+    BYTE4 = 3
+    BYTE8 = 4
+    LOCAL_IDX = 5
+    GLOBAL_IDX = 6
+    TABLE_IDX = 7
+    ELEM_IDX = 8
+    FUNC_IDX = 9
+    TYPE_IDX = 10
+    DATA_IDX = 11
+    LABEL_IDX = 12
+    VEC_LABEL_IDX = 13
+    BLOCK_TYPE = 14
+    SINT = 15
+
+
+@enum.unique
+class FLAGS(enum.IntFlag):
+    BLOCK_START = 1
+    BLOCK_END = 2
+
+
+###########################################################
+# OPCODES
+############################################################
+class Opcode:
+    Table: typing.Dict[int, "Opcode"] = {}
+
+    def __init__(self, no, name, arg1=ARG_TYPE.INVALID, arg2=ARG_TYPE.INVALID, arg3=ARG_TYPE.INVALID, flags=FLAGS(0)):
+
+        self.no = no
+        self.name = name
+        self.flags = flags
+        self.args = [a for a in (arg1, arg2, arg3) if a != ARG_TYPE.INVALID]
+        assert no not in Opcode.Table
+        Opcode.Table[no] = self
+
+
+def OpConst(no, name, arg):
+    return Opcode(no, name, arg)
+
+
+def OpVar(no, name, arg):
+    return Opcode(no, name, arg)
+
+
+def OpCmp(no, name):
+    return Opcode(no, name)
+
+
+def OpUnaryOp(no, name):
+    return Opcode(no, name)
+
+
+def OpBinOp(no, name):
+    return Opcode(no, name)
+
+
+def OpLoad(no, name):
+    return Opcode(no, name, ARG_TYPE.UINT, ARG_TYPE.UINT)
+
+
+def OpStore(no, name):
+    return Opcode(no, name, ARG_TYPE.UINT, ARG_TYPE.UINT)
+
+
+def OpTable(no, name, arg1, arg2=ARG_TYPE.INVALID):
+    return Opcode(no, name, arg1, arg2)
+
+
+def OpMem(no, name, arg1, arg2=ARG_TYPE.INVALID, arg3=ARG_TYPE.INVALID):
+    return Opcode(no, name, arg1, arg2, arg3)
+
+
+def OpCfg(no, name, arg1=ARG_TYPE.INVALID, arg2=ARG_TYPE.INVALID):
+    return Opcode(no, name, arg1, arg2)
+
+
+def OpConv(no, name):
+    return Opcode(no, name)
+
+
+def OpBlk(no, name, arg1=ARG_TYPE.INVALID):
+    return Opcode(no, name, arg1, flags=FLAGS.BLOCK_START)
+
+
+# control Instructions
+Opcode(0x00, 'unreachable')
+Opcode(0x01, 'nop')
+
+OpBlk(0x02, 'block', ARG_TYPE.BLOCK_TYPE)
+OpBlk(0x03, 'loop', ARG_TYPE.BLOCK_TYPE)
+OpBlk(0x04, 'if', ARG_TYPE.BLOCK_TYPE)
+Opcode(0x05, 'else')
+Opcode(0x0b, 'end', flags=FLAGS.BLOCK_END)
+
+OpCfg(0x0c, 'br', ARG_TYPE.LABEL_IDX)
+OpCfg(0x0d, 'br_if', ARG_TYPE.LABEL_IDX)
+OpCfg(0x0e, 'br_table', ARG_TYPE.VEC_LABEL_IDX, ARG_TYPE.LABEL_IDX)
+OpCfg(0x0f, 'return')
+OpCfg(0x10, 'call', ARG_TYPE.FUNC_IDX)
+OpCfg(0x11, 'call_indirect', ARG_TYPE.TYPE_IDX, ARG_TYPE.TABLE_IDX)
+
+# parametric Instructions
+Opcode(0x1a, 'drop')
+Opcode(0x1b, 'select')
+# op(0x1c, 'select_val')
+
+
+OpVar(0x20, 'local.get', ARG_TYPE.LOCAL_IDX)
+OpVar(0x21, 'local.set', ARG_TYPE.LOCAL_IDX)
+OpVar(0x22, 'local.tee', ARG_TYPE.LOCAL_IDX)
+OpVar(0x23, 'global.get', ARG_TYPE.GLOBAL_IDX)
+OpVar(0x24, 'global.set', ARG_TYPE.GLOBAL_IDX)
+
+OpTable(0x25, "table.get", ARG_TYPE.TABLE_IDX)
+OpTable(0x26, "table.set", ARG_TYPE.TABLE_IDX)
+OpTable(0x0cfc, "table.init", ARG_TYPE.ELEM_IDX, ARG_TYPE.TABLE_IDX)
+OpTable(0x0dfc, "table.drop", ARG_TYPE.ELEM_IDX)
+OpTable(0x0efc, "table.copy", ARG_TYPE.TABLE_IDX, ARG_TYPE.TABLE_IDX)
+OpTable(0x0ffc, "table.grow", ARG_TYPE.TABLE_IDX)
+OpTable(0x10fc, "table.size", ARG_TYPE.TABLE_IDX)
+OpTable(0x11fc, "table.fill", ARG_TYPE.TABLE_IDX)
+
+OpLoad(0x28, 'i32.load')
+OpLoad(0x29, 'i64.load')
+OpLoad(0x2a, 'f32.load')
+OpLoad(0x2b, 'f64.load')
+OpLoad(0x2c, 'i32.load8_s')
+OpLoad(0x2d, 'i32.load8_u')
+OpLoad(0x2e, 'i32.load16_s')
+OpLoad(0x2f, 'i32.load16_u')
+OpLoad(0x30, 'i64.load8_s')
+OpLoad(0x31, 'i64.load8_u')
+OpLoad(0x32, 'i64.load16_s')
+OpLoad(0x33, 'i64.load16_u')
+OpLoad(0x34, 'i64.load32_s')
+OpLoad(0x35, 'i64.load32_u')
+
+OpStore(0x36, 'i32.store')
+OpStore(0x37, 'i64.store')
+OpStore(0x38, 'f32.store')
+OpStore(0x39, 'f64.store')
+OpStore(0x3a, 'i32.store8')
+OpStore(0x3b, 'i32.store16')
+OpStore(0x3c, 'i64.store8')
+OpStore(0x3d, 'i64.store16')
+OpStore(0x3e, 'i64.store32')
+
+OpMem(0x3f, 'memory.size', ARG_TYPE.BYTE1_ZERO)
+OpMem(0x40, 'memory.grow', ARG_TYPE.BYTE1_ZERO)
+OpMem(0x08fc, 'memory.init', ARG_TYPE.DATA_IDX, ARG_TYPE.BYTE1_ZERO)
+OpMem(0x09fc, 'data.drop', ARG_TYPE.DATA_IDX)
+OpMem(0x0afc, 'memory.copy', ARG_TYPE.BYTE1_ZERO, ARG_TYPE.BYTE1_ZERO)
+OpMem(0x0bfc, 'memory.fill', ARG_TYPE.BYTE1_ZERO)
+
+# numeric instructions
+OpConst(0x41, 'i32.const', ARG_TYPE.SINT)
+OpConst(0x42, 'i64.const', ARG_TYPE.SINT)
+OpConst(0x43, 'f32.const', ARG_TYPE.BYTE4)
+OpConst(0x44, 'f64.const', ARG_TYPE.BYTE8)
+
+OpCmp(0x45, 'i32.eqz')
+OpCmp(0x46, 'i32.eq')
+OpCmp(0x47, 'i32.ne')
+OpCmp(0x48, 'i32.lt_s')
+OpCmp(0x49, 'i32.lt_u')
+OpCmp(0x4a, 'i32.gt_s')
+OpCmp(0x4b, 'i32.gt_u')
+OpCmp(0x4c, 'i32.le_s')
+OpCmp(0x4d, 'i32.le_u')
+OpCmp(0x4e, 'i32.ge_s')
+OpCmp(0x4f, 'i32.ge_u')
+OpCmp(0x50, 'i64.eqz')
+OpCmp(0x51, 'i64.eq')
+OpCmp(0x52, 'i64.ne')
+OpCmp(0x53, 'i64.lt_s')
+OpCmp(0x54, 'i64.lt_u')
+OpCmp(0x55, 'i64.gt_s')
+OpCmp(0x56, 'i64.gt_u')
+OpCmp(0x57, 'i64.le_s')
+OpCmp(0x58, 'i64.le_u')
+OpCmp(0x59, 'i64.ge_s')
+OpCmp(0x5a, 'i64.ge_u')
+OpCmp(0x5b, 'f32.eq')
+OpCmp(0x5c, 'f32.ne')
+OpCmp(0x5d, 'f32.lt')
+OpCmp(0x5e, 'f32.gt')
+OpCmp(0x5f, 'f32.le')
+OpCmp(0x60, 'f32.ge')
+OpCmp(0x61, 'f64.eq')
+OpCmp(0x62, 'f64.ne')
+OpCmp(0x63, 'f64.lt')
+OpCmp(0x64, 'f64.gt')
+OpCmp(0x65, 'f64.le')
+OpCmp(0x66, 'f64.ge')
+
+OpUnaryOp(0x67, 'i32.clz')
+OpUnaryOp(0x68, 'i32.ctz')
+OpUnaryOp(0x69, 'i32.popcnt')
+
+OpBinOp(0x6a, 'i32.add')
+OpBinOp(0x6b, 'i32.sub')
+OpBinOp(0x6c, 'i32.mul')
+OpBinOp(0x6d, 'i32.div_s')
+OpBinOp(0x6e, 'i32.div_u')
+OpBinOp(0x6f, 'i32.rem_s')
+OpBinOp(0x70, 'i32.rem_u')
+OpBinOp(0x71, 'i32.and')
+OpBinOp(0x72, 'i32.or')
+OpBinOp(0x73, 'i32.xor')
+OpBinOp(0x74, 'i32.shl')
+OpBinOp(0x75, 'i32.shr_s')
+OpBinOp(0x76, 'i32.shr_u')
+OpBinOp(0x77, 'i32.rotl')
+OpBinOp(0x78, 'i32.rotr')
+
+OpUnaryOp(0x79, 'i64.clz')
+OpUnaryOp(0x7a, 'i64.ctz')
+OpUnaryOp(0x7b, 'i64.popcnt')
+
+OpBinOp(0x7c, 'i64.add')
+OpBinOp(0x7d, 'i64.sub')
+OpBinOp(0x7e, 'i64.mul')
+OpBinOp(0x7f, 'i64.div_s')
+OpBinOp(0x80, 'i64.div_u')
+OpBinOp(0x81, 'i64.rem_s')
+OpBinOp(0x82, 'i64.rem_u')
+OpBinOp(0x83, 'i64.and')
+OpBinOp(0x84, 'i64.or')
+OpBinOp(0x85, 'i64.xor')
+OpBinOp(0x86, 'i64.shl')
+OpBinOp(0x87, 'i64.shr_s')
+OpBinOp(0x88, 'i64.shr_u')
+OpBinOp(0x89, 'i64.rotl')
+OpBinOp(0x8a, 'i64.rotr')
+
+OpUnaryOp(0x8b, 'f32.abs')
+OpUnaryOp(0x8c, 'f32.neg')
+OpUnaryOp(0x8d, 'f32.ceil')
+OpUnaryOp(0x8e, 'f32.floor')
+OpUnaryOp(0x8f, 'f32.trunc')
+OpUnaryOp(0x90, 'f32.nearest')
+OpUnaryOp(0x91, 'f32.sqrt')
+
+OpBinOp(0x92, 'f32.add')
+OpBinOp(0x93, 'f32.sub')
+OpBinOp(0x94, 'f32.mul')
+OpBinOp(0x95, 'f32.div')
+OpBinOp(0x96, 'f32.min')
+OpBinOp(0x97, 'f32.max')
+
+OpUnaryOp(0x98, 'f32.copysign')
+OpUnaryOp(0x99, 'f64.abs')
+OpUnaryOp(0x9a, 'f64.neg')
+OpUnaryOp(0x9b, 'f64.ceil')
+OpUnaryOp(0x9c, 'f64.floor')
+OpUnaryOp(0x9d, 'f64.trunc')
+OpUnaryOp(0x9e, 'f64.nearest')
+OpUnaryOp(0x9f, 'f64.sqrt')
+
+OpBinOp(0xa0, 'f64.add')
+OpBinOp(0xa1, 'f64.sub')
+OpBinOp(0xa2, 'f64.mul')
+OpBinOp(0xa3, 'f64.div')
+OpBinOp(0xa4, 'f64.min')
+OpBinOp(0xa5, 'f64.max')
+
+OpUnaryOp(0xa6, 'f64.copysign')
+
+OpConv(0xa7, 'i32.wrap_i64')
+OpConv(0xa8, 'i32.trunc_f32_s')
+OpConv(0xa9, 'i32.trunc_f32_u')
+OpConv(0xaa, 'i32.trunc_f64_s')
+OpConv(0xab, 'i32.trunc_f64_u')
+OpConv(0xac, 'i64.extend_i32_s')
+OpConv(0xad, 'i64.extend_i32_u')
+OpConv(0xae, 'i64.trunc_f32_s')
+OpConv(0xaf, 'i64.trunc_f32_u')
+OpConv(0xb0, 'i64.trunc_f64_s')
+OpConv(0xb1, 'i64.trunc_f64_u')
+OpConv(0xb2, 'f32.convert_i32_s')
+OpConv(0xb3, 'f32.convert_i32_u')
+OpConv(0xb4, 'f32.convert_i64_s')
+OpConv(0xb5, 'f32.convert_i64_u')
+OpConv(0xb6, 'f32.demote_f64')
+OpConv(0xb7, 'f64.convert_i32_s')
+OpConv(0xb8, 'f64.convert_i32_u')
+OpConv(0xb9, 'f64.convert_i64_s')
+OpConv(0xba, 'f64.convert_i64_u')
+OpConv(0xbb, 'f64.promote_f32')
+
+OpConv(0xbc, 'i32.reinterpret_f32')
+OpConv(0xbd, 'i64.reinterpret_f64')
+OpConv(0xbe, 'f32.reinterpret_i32')
+OpConv(0xbf, 'f64.reinterpret_i64')
+
+if __name__ == '__main__':
+    def dump():
+        for no, opcode in sorted(Opcode.Table.items()):
+            args = [a.name for a in opcode.args]
+            print(f"{opcode.no:02x} {opcode.name:20} {' '.join(args)}")
+
+
+    dump()
---
+++ FrontEndWASM/parser.py
@@ -0,0 +1,514 @@
+#!/usr/bin/python3
+
+"""
+Parser for WASM files
+
+Spec:
+https://webassembly.github.io/spec/core/syntax/types.html
+"""
+
+import logging
+import io
+import typing
+import enum
+import dataclasses
+from FrontEndWASM.opcode_tab import Opcode, ARG_TYPE, FLAGS
+
+
+def read_leb128(r: typing.BinaryIO, signed: bool = False) -> int:
+    """
+    cf. http://en.wikipedia.org/wiki/LEB128
+    """
+    out = 0
+    shift = 0
+    while True:
+        b = ord(r.read(1))
+        out |= (b & 0x7f) << shift
+        shift += 7
+        if (b & 0x80) == 0:
+            if signed and b & 0x40:
+                out -= (1 << shift)
+            return out
+
+
+def read_bytes(r: typing.BinaryIO) -> bytes:
+    n = read_leb128(r)
+    data = r.read(n)
+    assert len(data) == n
+    return data
+
+
+def read_string(r: typing.BinaryIO) -> str:
+    return read_bytes(r).decode()
+
+
+def read_vec(r: typing.BinaryIO, cls) -> typing.List:
+    n = read_leb128(r)
+    return [cls.read(r) for _ in range(n)]
+
+
+############################################################
+# TYPES
+# https://webassembly.github.io/spec/core/binary/types.html
+############################################################
+
+
+@enum.unique
+class NUM_TYPE(enum.IntEnum):
+    F64 = 0x7c
+    F32 = 0x7d
+    I64 = 0x7e
+    I32 = 0x7f
+
+
+@enum.unique
+class REF_TYPE(enum.IntEnum):
+    EXTERNREF = 0x6f
+    FUNCREF = 0x70
+
+
+@enum.unique
+class VAL_TYPE(enum.IntEnum):
+    EXTERNREF = 0x6f
+    FUNCREF = 0x70
+    F64 = 0x7c
+    F32 = 0x7d
+    I64 = 0x7e
+    I32 = 0x7f
+
+
+@enum.unique
+class MUT(enum.IntEnum):
+    CONST = 0
+    VAR = 1
+
+
+@enum.unique
+class EXTERN(enum.IntEnum):
+    FUNCTION = 0
+    TABLE = 1
+    MEMORY = 2
+    GLOBAL = 3
+
+
+@dataclasses.dataclass(frozen=True)
+class ResultType:
+    typed: typing.List[VAL_TYPE] = dataclasses.field(default_factory=list)
+
+    @classmethod
+    def read(cls, r: typing.BinaryIO):
+        n = read_leb128(r)
+        return ResultType([VAL_TYPE(ord(r.read(1))) for _ in range(n)])
+
+
+@dataclasses.dataclass(frozen=True)
+class FunctionType:
+    args: ResultType
+    rets: ResultType
+
+    @classmethod
+    def read(cls, r: typing.BinaryIO):
+        s = ord(r.read(1))
+        assert s == 0x60
+        return FunctionType(ResultType.read(r), ResultType.read(r))
+
+
+@dataclasses.dataclass(frozen=True)
+class Limits:
+    min: int
+    max: int
+
+    @classmethod
+    def read(cls, r: typing.BinaryIO):
+        flag = ord(r.read(1))
+        a_min = read_leb128(r)
+        a_max = read_leb128(r) if flag else 0x00
+        return Limits(a_min, a_max)
+
+
+@dataclasses.dataclass(frozen=True)
+class MemoryType:
+    limits: Limits
+
+    @classmethod
+    def read(cls, r: typing.BinaryIO):
+        return MemoryType(Limits.read(r))
+
+
+@dataclasses.dataclass(frozen=True)
+class TableType:
+    element_type: ResultType
+    limits: Limits
+
+    @classmethod
+    def read(cls, r: typing.BinaryIO):
+        return TableType(REF_TYPE(ord(r.read(1))), Limits.read(r))
+
+
+@dataclasses.dataclass(frozen=True)
+class GlobalType:
+    value_type: VAL_TYPE
+    mut: MUT
+
+    @classmethod
+    def read(cls, r: typing.BinaryIO):
+        return GlobalType(VAL_TYPE(ord(r.read(1))), MUT(ord(r.read(1))))
+
+
+# https://webassembly.github.io/spec/core/syntax/types.html#external-types
+ExternalType = typing.Union[FunctionType, TableType, MemoryType, GlobalType]
+
+
+def ReadArg(r: typing.BinaryIO, at: ARG_TYPE) -> typing.Any:
+    if at is ARG_TYPE.UINT:
+        return read_leb128(r)
+    elif at is ARG_TYPE.SINT:
+        return read_leb128(r, True)
+    elif at is ARG_TYPE.BYTE1_ZERO:
+        return ord(r.read(1))
+    elif at is ARG_TYPE.BYTE4:
+        return r.read(4)
+    elif at is ARG_TYPE.BYTE8:
+        return r.read(8)
+    elif at is ARG_TYPE.LOCAL_IDX:
+        return LocalIdx.read(r)
+    elif at is ARG_TYPE.GLOBAL_IDX:
+        return GlobalIdx.read(r)
+    elif at is ARG_TYPE.TABLE_IDX:
+        return TableIdx.read(r)
+    elif at is ARG_TYPE.ELEM_IDX:
+        return ElemIdx.read(r)
+    elif at is ARG_TYPE.FUNC_IDX:
+        return FuncIdx.read(r)
+    elif at is ARG_TYPE.TYPE_IDX:
+        return TypeIdx.read(r)
+    elif at is ARG_TYPE.DATA_IDX:
+        return DataIdx.read(r)
+    elif at is ARG_TYPE.LABEL_IDX:
+        return LabelIdx.read(r)
+    elif at is ARG_TYPE.VEC_LABEL_IDX:
+        return read_vec(r, LabelIdx)
+    elif at is ARG_TYPE.BLOCK_TYPE:
+        block_type = read_leb128(r, True)
+        if block_type > 0:
+            return TypeIdx(block_type)
+        block_type += 0x80
+        if block_type == 0x40:
+            return None
+        return VAL_TYPE(block_type)
+    else:
+        assert False
+
+
+@dataclasses.dataclass(frozen=True)
+class Instruction:
+    opcode: Opcode
+    args: typing.List[typing.Any]
+
+    def __repr__(self):
+        return f"{self.opcode.name} {' '.join([repr(a) for a in self.args])}"
+
+    @classmethod
+    def read(cls, r: typing.BinaryIO):
+        opcode = Opcode.Table[ord(r.read(1))]
+        args = [ReadArg(r, kind) for kind in opcode.args]
+        return Instruction(opcode, args)
+
+
+###########################################################
+# INDICES
+# https://webassembly.github.io/spec/core/binary/modules.html#indices
+############################################################
+
+class Idx(int):
+    def __repr__(self):
+        return f'{type(self).__name__}({super().__repr__()})'
+
+    @classmethod
+    def read(cls, r: typing.BinaryIO):
+        return cls(read_leb128(r))
+
+
+class TypeIdx(Idx):
+    pass
+
+
+class FuncIdx(Idx):
+    pass
+
+
+class TableIdx(Idx):
+    pass
+
+
+class MemIdx(Idx):
+    pass
+
+
+class GlobalIdx(Idx):
+    pass
+
+
+class ElemIdx(Idx):
+    pass
+
+
+class DataIdx(Idx):
+    pass
+
+
+class LocalIdx(Idx):
+    pass
+
+
+class LabelIdx(Idx):
+    pass
+
+###########################################################
+# SECTIONS Elements
+# https://webassembly.github.io/spec/core/binary/modules.html#sections
+############################################################
+
+ImportDesc = typing.Union[TypeIdx, TableType, MemoryType, GlobalType]
+
+
+@dataclasses.dataclass(frozen=True)
+class Import:
+    module: str
+    name: str
+    desc: ImportDesc
+
+    @classmethod
+    def read(cls, r: typing.BinaryIO):
+        module = read_string(r)
+        name = read_string(r)
+        kind = EXTERN(ord(r.read(1)))
+        desc = {
+            EXTERN.FUNCTION: TypeIdx.read,
+            EXTERN.TABLE: TableType.read,
+            EXTERN.MEMORY: MemoryType.read,
+            EXTERN.GLOBAL: GlobalType.read,
+        }[kind](r)
+        return Import(module, name, desc)
+
+
+@dataclasses.dataclass(frozen=True)
+class Table:
+    table_type: TableType
+
+    @classmethod
+    def read(cls, r: typing.BinaryIO):
+        return Table(TableType.read(r))
+
+
+@dataclasses.dataclass(frozen=True)
+class Mem:
+    mem_type: MemoryType
+
+    @classmethod
+    def read(cls, r: typing.BinaryIO):
+        return Mem(MemoryType.read(r))
+
+
+@dataclasses.dataclass(frozen=True)
+class Expression:
+    instructions: typing.List[Instruction]
+
+    @classmethod
+    def read(cls, r: typing.BinaryIO):
+        instructions = []
+        nesting = 1
+        while True:
+            i = Instruction.read(r)
+            # print ("@@", nesting, i)
+            instructions.append(i)
+            if FLAGS.BLOCK_START in i.opcode.flags:
+                nesting += 1
+            if FLAGS.BLOCK_END in i.opcode.flags:
+                nesting -= 1
+            if nesting == 0:
+                break
+
+        return Expression(instructions)
+
+
+@dataclasses.dataclass(frozen=True)
+class Glob:
+    global_type: GlobalType
+    expr: Expression
+
+    @classmethod
+    def read(cls, r: typing.BinaryIO):
+        return Glob(GlobalType.read(r), Expression.read(r))
+
+
+@dataclasses.dataclass(frozen=True)
+class Export:
+    name: str
+    desc: typing.Union[FuncIdx, TableIdx, MemIdx, GlobalIdx]
+
+    @classmethod
+    def read(cls, r: typing.BinaryIO):
+        name = read_string(r)
+        kind = EXTERN(ord(r.read(1)))
+        desc = {
+            EXTERN.FUNCTION: FuncIdx.read,
+            EXTERN.TABLE: TableIdx.read,
+            EXTERN.MEMORY: MemIdx.read,
+            EXTERN.GLOBAL: GlobalIdx.read,
+        }[kind](r)
+        return Export(name, desc)
+
+
+@dataclasses.dataclass(frozen=True)
+class Locals:
+    count: int
+    kind: NUM_TYPE
+
+    @classmethod
+    def read(cls, r: typing.BinaryIO):
+        count = read_leb128(r)
+        kind = VAL_TYPE(ord(r.read(1)))
+        return Locals(count, kind)
+
+
+@dataclasses.dataclass(frozen=True)
+class Code:
+    local_list: typing.List[Locals]
+    expr: Expression
+
+    @classmethod
+    def read(cls, rr: typing.BinaryIO):
+        r = io.BytesIO(read_bytes(rr))
+        local_list = read_vec(r, Locals)
+        expr = Expression.read(r)
+        rest = r.read(-1)
+        if rest:
+            raise Exception(f'not all code data was for Func: {rest}')
+        return Code(local_list, expr)
+
+
+@dataclasses.dataclass(frozen=True)
+class Data:
+    memory_index: typing.Optional[MemIdx]
+    offset: typing.Optional[Expression]
+    init: bytes
+
+    @classmethod
+    def read(cls, r: typing.BinaryIO):
+        flags = ord(r.read(1))
+        if flags == 0:
+            return Data(MemIdx(0), Expression.read(r), read_bytes(r))
+        elif flags == 1:
+            return Data(None, None, read_bytes(r))
+        elif flags == 2:
+            return Data(MemIdx.read(r), Expression.read(r), read_bytes(r))
+        else:
+            assert False
+
+
+# TODO
+@dataclasses.dataclass(frozen=True)
+class Elem:
+    # The initial contents of a table is uninitialized. The elem component of a module defines a vector of element
+    # segments that initialize a subrange of a table, at a given offset, from a static vector of elements.
+    #
+    # elem ::= {table tableidx, offset expr, init vec(funcidx)}
+    #
+    # The offset is given by a constant expression.
+    table_index: TableIdx
+    offset: Expression
+    init: typing.List[FuncIdx]
+
+    @classmethod
+    def read(cls, r: typing.BinaryIO):
+        table_index = TableIdx.read(r)
+        offset = Expression.read(r)
+        n = read_leb128(r)
+        init = [FuncIdx.read(r) for _ in range(n)]
+        return Elem(table_index, offset, init)
+
+
+###########################################################
+# SECTIONS
+# https://webassembly.github.io/spec/core/binary/modules.html#sections
+############################################################
+@enum.unique
+class SECTION_ID(enum.IntEnum):
+    CUSTOM = 0x0
+    TYPE = 0x1
+    IMPORT = 0x2
+    FUNCTION = 0x3
+    TABLE = 0x4
+    MEMORY = 0x5
+    GLOBAL = 0x6
+    EXPORT = 0x7
+    START = 0x8
+    ELEMENT = 0x9
+    CODE = 0xa
+    DATA = 0xb
+    DATA_COUNT = 0xc
+
+
+@dataclasses.dataclass(frozen=True)
+class Section:
+    no: SECTION_ID
+    # usually a list except for Custom and Start Sections
+    items: typing.Any
+
+
+_sec_id_to_reader = {
+    SECTION_ID.CUSTOM: lambda r: (read_string(r), bytes(r.read(-1))),
+    SECTION_ID.TYPE: lambda r: read_vec(r, FunctionType),
+    SECTION_ID.IMPORT: lambda r: read_vec(r, Import),
+    SECTION_ID.FUNCTION: lambda r: read_vec(r, TypeIdx),
+    SECTION_ID.TABLE: lambda r: read_vec(r, Table),
+    SECTION_ID.MEMORY: lambda r: read_vec(r, Mem),
+    SECTION_ID.GLOBAL: lambda r: read_vec(r, Glob),
+    SECTION_ID.EXPORT: lambda r: read_vec(r, Export),
+    SECTION_ID.START: lambda r: FuncIdx.read(r),
+    SECTION_ID.ELEMENT: lambda r: read_vec(r, Elem),
+    SECTION_ID.CODE: lambda r: read_vec(r, Code),
+    SECTION_ID.DATA: lambda r: read_vec(r, Data),
+    SECTION_ID.DATA_COUNT: lambda r: 0,
+}
+
+
+@dataclasses.dataclass(frozen=True)
+class Module:
+    version: typing.List[int]
+    sections: typing.Dict[SECTION_ID, typing.List[Section]] = dataclasses.field(default_factory=dict)
+
+    @classmethod
+    def read(cls, r: typing.BinaryIO):
+        header = r.read(4)
+        if header != b"\x00asm":
+            raise Exception(f'no magic wasm header detected: {header}')
+        version = list(r.read(4))
+        # if version != [0x01, 0x00, 0x00, 0x00]:
+        #    raise Exception(f'unknown binary version: {version}')
+
+        sections: typing.Dict[SECTION_ID, typing.List[Section]] = {}
+
+        while True:
+            id_byte = r.read(1)
+            if not id_byte:
+                break
+            sec_id = SECTION_ID(ord(id_byte))
+            logging.info(f"Reading section of type {sec_id.name}")
+            io_data = io.BytesIO(read_bytes(r))
+            data = _sec_id_to_reader[sec_id](io_data)
+            sections[sec_id] = Section(sec_id, data)
+            # print (sections[sec_id])
+            if io_data.read(1):
+                raise Exception(f'not all section data was consumed for {sec_id}')
+
+        return Module(version, sections)
+
+
+if __name__ == '__main__':
+    import sys
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info(f"Reading {sys.argv[1]}")
+    with open(sys.argv[1], "rb") as fin:
+        Module.read(fin)
