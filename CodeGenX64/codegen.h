e332c5491c874da570352c08f266645187db25eb
Robert Muth <robert@muth.org>
2022-01-31 20:36:40 -0500

    x64 c++ fix build

--- CodeGenX64/codegen.h
+++ CodeGenX64/codegen.h
@@ -1,15 +1,15 @@
 #pragma once
 // (c) Robert Muth - see LICENSE for more info
 
+#include <iostream>
 #include "Base/ir.h"
 #include "CpuX64/assembler.h"
-#include <iostream>
 
 namespace cwerg::code_gen_x64 {
 using namespace cwerg;
 
 extern void EmitUnitAsText(base::Unit unit, std::ostream* output);
 
-extern x64::A64Unit EmitUnitAsBinary(base::Unit unit, bool add_startup_code);
+extern x64::X64Unit EmitUnitAsBinary(base::Unit unit, bool add_startup_code);
 
 }  // namespace cwerg::code_gen_x64
