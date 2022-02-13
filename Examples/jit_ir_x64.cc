c2f75b840049ef21e54f7787ee7906d6bb82312d
Robert Muth <robert@muth.org>
2022-02-13 09:32:38 -0500

    add x64 jit example

---
+++ Examples/jit_ir_x64.cc
@@ -0,0 +1,111 @@
+// This examples shows how to jit cwerg ir to a64 assembly
+
+#include "Base/ir.h"
+#include "CodeGenX64/codegen.h"
+#include "CodeGenX64/legalize.h"
+#include "CpuX64/symbolic.h"
+#include "Util/assert.h"
+
+#include <sys/mman.h>
+#include <iostream>
+
+using namespace cwerg;
+
+// Create a Unit with a single function "fib" modelled after ../TestData/fib.asm
+base::Unit MakeFibonacci() {
+  using namespace cwerg::base;
+
+  Unit unit = UnitNew(StrNew("fibonacci"));
+  Fun fun = UnitFunAdd(unit, FunNew(StrNew("fib"), FUN_KIND::NORMAL));
+  // setup function signature
+  FunOutputTypes(fun)[0] = DK::U32;
+  FunNumOutputTypes(fun) = 1;
+  FunInputTypes(fun)[0] = DK::U32;
+  FunNumInputTypes(fun) = 1;
+  Bbl bbl_start = FunBblAdd(fun, BblNew(StrNew("start")));
+  Bbl bbl_difficult = FunBblAdd(fun, BblNew(StrNew("difficult")));
+  Reg reg_in = FunRegAdd(fun, RegNew(DK::U32, StrNew("in")));
+  Reg reg_out = FunRegAdd(fun, RegNew(DK::U32, StrNew("out")));
+  Reg reg_x = FunRegAdd(fun, RegNew(DK::U32, StrNew("x")));
+  Const zero = ConstNewU(DK::U32, 0);
+  Const one = ConstNewU(DK::U32, 1);
+  Const two = ConstNewU(DK::U32, 2);
+  // populate Bbl start
+  BblInsAdd(bbl_start, InsNew(OPC::POPARG, reg_in));
+  BblInsAdd(bbl_start, InsNew(OPC::BLT, one, reg_in, bbl_difficult));
+  BblInsAdd(bbl_start, InsNew(OPC::PUSHARG, reg_in));
+  BblInsAdd(bbl_start, InsNew(OPC::RET));
+  // populate Bbl difficult
+  BblInsAdd(bbl_difficult, InsNew(OPC::MOV, reg_out, zero));
+  BblInsAdd(bbl_difficult, InsNew(OPC::SUB, reg_x, reg_in, one));
+  BblInsAdd(bbl_difficult, InsNew(OPC::PUSHARG, reg_x));
+  BblInsAdd(bbl_difficult, InsNew(OPC::BSR, fun));
+  BblInsAdd(bbl_difficult, InsNew(OPC::POPARG, reg_x));
+  BblInsAdd(bbl_difficult, InsNew(OPC::ADD, reg_out, reg_out, reg_x));
+  BblInsAdd(bbl_difficult, InsNew(OPC::SUB, reg_x, reg_in, two));
+  BblInsAdd(bbl_difficult, InsNew(OPC::PUSHARG, reg_x));
+  BblInsAdd(bbl_difficult, InsNew(OPC::BSR, fun));
+  BblInsAdd(bbl_difficult, InsNew(OPC::POPARG, reg_x));
+  BblInsAdd(bbl_difficult, InsNew(OPC::ADD, reg_out, reg_out, reg_x));
+  BblInsAdd(bbl_difficult, InsNew(OPC::PUSHARG, reg_out));
+  BblInsAdd(bbl_difficult, InsNew(OPC::RET));
+
+  return unit;
+}
+
+using FunPtr = uint32_t (*)(uint32_t);
+
+int main(int argc, char* argv[]) {
+  // Set-up internal memory pools.
+  InitStripes(1);
+  code_gen_x64::InitCodeGenX64();
+
+  // Generate IR and translate to A64
+  base::Unit unit = MakeFibonacci();
+  code_gen_x64::LegalizeAll(unit, false, nullptr);
+  code_gen_x64::RegAllocGlobal(unit, false, nullptr);
+  code_gen_x64::RegAllocLocal(unit, false, nullptr);
+  x64::X64Unit x64unit = code_gen_x64::EmitUnitAsBinary(unit, false);
+  std::cerr << std::hex;
+  std::cerr
+            << "RODATA SIZE: 0x" << x64unit.sec_rodata->data->size() << "\n"
+            << "TEXT SIZE:   0x" << x64unit.sec_text->data->size() << "\n"
+            << "DATA SIZE:   0x" << x64unit.sec_data->data->size() << "\n"
+            << "BSS SIZE:    0x" << x64unit.sec_bss->data->size() << "\n";
+
+  // Allocate space for Section data
+  uint32_t* text_memory =
+      (uint32_t*)mmap(nullptr, 4096, PROT_READ | PROT_WRITE | PROT_EXEC,
+                      MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+  ASSERT(text_memory != nullptr, "");
+  x64unit.sec_text->shdr.sh_addr = (uint64_t)text_memory;
+  std::cerr << "text addr: " << text_memory << "\n";
+
+  // Compute Symbol Addresses and Relocate
+  for (auto& sym : x64unit.symbols) {
+    ASSERT(sym->sym.st_value != ~0U, "undefined symbol " << sym->name);
+    if (sym->section != nullptr) {
+      ASSERT(sym->section->shdr.sh_addr != ~0U,
+             sym->name << "has bad sec " << *sym->section);
+      sym->sym.st_value += sym->section->shdr.sh_addr;
+      std::cerr << "sym [" << sym->name << "]: 0x" <<   sym->sym.st_value << "\n";
+    }
+  }
+
+  for (auto& rel : x64unit.relocations) {
+    x64::ApplyRelocation(rel);
+  }
+
+  // Copy Section data to their final destination
+  // Note we only have a text section
+  memcpy(text_memory, x64unit.sec_text->data->data(), x64unit.sec_text->data->size());
+
+  // Invoke JITed code. This assumes that the calling convention of the
+  // JITed code matches the platform ABI. This is only true for simple cases.
+  FunPtr f = reinterpret_cast<FunPtr>(text_memory);
+  for (unsigned i = 0; i < 10; ++i) {
+    std::cout << std::dec << i << " " << f(i) << "\n";
+  }
+  return 0;
+}
