06959b88f39814b898ac0e8aa70bda671bfdff41
Robert Muth <robert@muth.org>
2022-09-10 00:01:33 -0400

    dwarf line number generator works begins

--- Dwarf/dump_line_numbers.py
+++ Dwarf/dump_line_numbers.py
@@ -146,20 +146,56 @@ def ReadEntryList(data) -> Tuple[List, List]:
     return format, entries
 
 
-@dataclasses.dataclass
+class DeltaEncoder:
+
+    def __init__(self, opcode_base, line_base, line_range, min_instruction_length):
+        self._opcode_base = opcode_base
+        self._line_base = line_base
+        self._line_range = line_range
+        self._min_instruction_length = min_instruction_length
+        self._max_ad_normalized = (
+            255 - opcode_base) // line_range * line_range
+        self.max_ad = self._max_ad_normalized // line_range * min_instruction_length
+
+    def ld_within_range(self, ld):
+        return ld >= self._line_base and ld < self._line_base + self._line_range
+
+    def code_from_delta(self, ad, ld) -> Tuple[int, bool]:
+        if not self.ld_within_range(ld):
+            return None, None
+        assert ad % self._min_instruction_length == 0
+        a = ld - self._line_base
+        b = ad // self._min_instruction_length * self._line_range
+        out = a + b + self._opcode_base
+        if out <= 255:
+            return out, False
+        out -= self._max_ad_normalized  # pretend we have a DW_LNS_const_add_pc
+        if out <= 255:
+            return out, True
+        return None, None
+
+    def deltas_from_code(self, x):
+        xx = x - self._opcode_base
+        ad = xx // self._line_range * self._min_instruction_length
+        ld = self._line_base + xx % self._line_range
+        assert self.code_from_delta(ad, ld)[0] == x, f"{x} {ad} {ld}"
+        return ad, ld
+
+
+@ dataclasses.dataclass
 class StateMachine:
     address: int = 0
     # op_index:int  = 0  # irrelevant for our purposes- vliw only
     file: int = 1
     line: int = 1
     column: int = 0
-    is_stmt = True
-    basic_block = False
-    end_sequence = False
-    prologue_end = False
-    epilogue_begin = False
-    isa = 0
-    discriminator = 0
+    is_stmt: bool = True
+    basic_block:  bool = False
+    end_sequence: bool = False
+    prologue_end: bool = False
+    epilogue_begin: bool = False
+    isa: int = 0
+    discriminator: int = 0
 
     def clear(self):
         self.discriminator = 0
@@ -167,20 +203,17 @@ class StateMachine:
         self.prologue_end = False
         self.epilogue_begin = False
 
-    @staticmethod
-    def New(default_is_stmt):
+    @ staticmethod
+    def New(default_is_stmt: bool):
         out = StateMachine()
         out.is_stmt = default_is_stmt
         return out
 
 
-def ReadCommands(end, data: io.BytesIO, default_is_stmt, opcode_base, line_base, line_range,
-                 min_instruction_length, maximum_operations_per_instruction):
+def ReadCommands(end, data: io.BytesIO, delta_enc, default_is_stmt: bool,
+                 opcode_base: int) -> List[StateMachine]:
     sm = StateMachine.New(default_is_stmt)
-    matrix = []
-
-    def addr_delta(x):
-        return ((x - opcode_base) // line_range) * min_instruction_length // maximum_operations_per_instruction
+    out: List[StateMachine] = []
 
     while data.tell() < end:
         print(f"[0x{data.tell():08x}]  ", end="")
@@ -196,7 +229,10 @@ def ReadCommands(end, data: io.BytesIO, default_is_stmt, opcode_base, line_base,
             elif x is DW_LNE.end_sequence:
                 print(f"Extended opcode {op}: End of Sequence\n")
                 sm.end_sequence = True
+                out.append(dataclasses.replace(sm))
                 sm = StateMachine.New(default_is_stmt)
+                # if data.tell() > 900: return out  # HACK
+                # return out
             elif x is DW_LNE.set_discriminator:
                 sm.discriminator = read_leb128(data)
                 print(
@@ -219,7 +255,10 @@ def ReadCommands(end, data: io.BytesIO, default_is_stmt, opcode_base, line_base,
                 print(f"Advance Line by {v} to {sm.line}")
             elif x is DW_LNS.copy:
                 print(f"Copy")
+                out.append(dataclasses.replace(sm))
+                assert out[-1].is_stmt == sm.is_stmt, f"{sm}"
                 sm.clear()
+
             elif x is DW_LNS.negate_stmt:
                 sm.is_stmt = not sm.is_stmt
                 print(f"Set is_stmt to {int(sm.is_stmt)}")
@@ -228,26 +267,110 @@ def ReadCommands(end, data: io.BytesIO, default_is_stmt, opcode_base, line_base,
                 print(
                     f"Set File Name to entry {sm.file} in the File Name Table")
             elif x is DW_LNS.const_add_pc:
-                x = 255 - opcode_base
-                delta = addr_delta(255)
-                sm.address += delta
-                sm.clear()
-                print(f"Advance PC by constant {delta} to 0x{sm.address:x}")
+                ad, _ = delta_enc.deltas_from_code(255)
+                sm.address += ad
+                print(f"Advance PC by constant {ad} to 0x{sm.address:x}")
             else:
                 assert False, x
         else:
             # special
-            ad = addr_delta(op)
+            ad, ld = delta_enc.deltas_from_code(op)
             sm.address += ad
-            ld = line_base + (op - opcode_base) % line_range
             sm.line += ld
+            out.append(dataclasses.replace(sm))
             sm.clear()
             print(f"Special opcode {op - opcode_base}: advance Address by {ad} to 0x{sm.address:x} "
                   f"and Line by {ld} to {sm.line}")
 
+    return out
 
-@dataclasses.dataclass
-class DebugLineHeader:
+
+def GenerateCommands(matrix: List[StateMachine], de: DeltaEncoder, default_is_stmt: bool,
+                     opcode_base: int):
+    # max_line
+    curr = StateMachine.New(default_is_stmt)
+    while matrix:
+        next = matrix.pop(0)
+        if next.file != curr.file:
+            curr.file = next.file
+            print(
+                f"@@@ Set File Name to entry {curr.file} in the File Name Table")
+        
+        if next.column != curr.column:
+            print(f"@@@ Set column to {next.column}")
+            curr.column = next.column
+        if next.discriminator != curr.discriminator:
+            curr.discriminator = next.discriminator
+            print(
+                f"@@@ Extended opcode 4: set Discriminator to {curr.discriminator}")
+
+        if next.is_stmt != curr.is_stmt:
+            curr.is_stmt = not curr.is_stmt
+            print(f"@@@ Set is_stmt to {int(curr.is_stmt)}")
+
+        if curr.address == 0:
+            print(
+                f"@@@ Extended opcode {DW_LNE.set_address}: set Address to 0x{next.address:x}")
+            ld = next.line - curr.line
+            print(f"@@@ Advance Line by {ld} to {next.line}")
+            print(f"@@@ Copy")
+            curr.address = next.address
+            curr.line = next.line
+            assert curr == next
+            curr.clear()
+            continue
+
+        ad = next.address - curr.address
+        ld = next.line - curr.line
+        if not next.end_sequence:
+            if not de.ld_within_range(ld):
+                print(f"@@@ Advance Line by {ld} to {next.line}")
+                curr.line += ld
+                ld = 0
+            if ad == 0 and ld == 0:
+                print(f"@@@ Copy")
+                assert curr == next
+                curr.clear()
+                continue
+
+            opc, const_add_pc = de.code_from_delta(ad, ld)
+
+            if opc is None:
+                curr.address += ad
+                print(f"@@@ Advance PC by {ad} to 0x{curr.address:x}")
+                ad = 0
+                opc, const_add_pc = de.code_from_delta(ad, ld)
+
+            if const_add_pc:
+                curr.address += de.max_ad
+                ad -= de.max_ad
+                print(
+                    f"@@@ Advance PC by constant {de.max_ad} to 0x{curr.address:x}")
+            print(
+                f"@@@ Special opcode {opc - opcode_base}: advance Address by {ad} to 0x{next.address:x} and Line by {ld} to {next.line}")
+            curr.address += ad
+            curr.line += ld
+            assert curr == next
+            curr.clear()
+            continue
+
+        if ad != 0:
+            print(f"@@@ Advance PC by {ad} to 0x{next.address:x}")
+            curr.address += ad
+
+        if ld != 0:
+            print(f"@@@ Advance Line by {ld} to {next.line}")
+            curr.line += ld
+
+        curr.end_sequence = True
+        assert curr == next
+        print("@@@ Extended opcode 1: End of Sequence")
+        curr = StateMachine.New(default_is_stmt)
+        print()
+
+
+@ dataclasses.dataclass
+class LineNumberProgram:
     """An Elf Symbol"""
     length: int = 0
     version: int = 0
@@ -272,6 +395,9 @@ class DebugLineHeader:
     file_names: List[List[int]] = dataclasses.field(
         default_factory=list)
 
+    commands: List = dataclasses.field(
+        default_factory=list)
+
     FORMAT = "<IHBBIBBBbB"
     SIZE = struct.calcsize(FORMAT)
 
@@ -286,17 +412,24 @@ class DebugLineHeader:
          self.maximum_operations_per_instruction,
          self.default_is_stmt,
          self.line_base, self.line_range,) = struct.unpack(
-            DebugLineHeader.FORMAT, data.read(DebugLineHeader.SIZE))
-
+            LineNumberProgram.FORMAT, data.read(LineNumberProgram.SIZE))
+        assert self.maximum_operations_per_instruction == 1, "VLIW not supported"
+        assert self.version == 5, f"unknown version {self.version}"
         self.opcode_base = ord(data.read(1))
         self.std_opcode_lengths = list(data.read(self.opcode_base - 1))
         #
         self.directory_entry_format, self.directories = ReadEntryList(data)
         self.file_name_entry_format, self.file_names = ReadEntryList(data)
+        de = DeltaEncoder(self.opcode_base, self.line_base,
+                          self.line_range, self.min_instruction_length)
+        print(f"# opcode_base={self.opcode_base} min_instruction_length={self.min_instruction_length} "
+              f"line_base={self.line_base} line_range={self.line_range} default_is_stmt={self.default_is_stmt}")
         self.commands = ReadCommands(start + 4 + self.length,
-                                     data, self.default_is_stmt, self.opcode_base, self.line_base, self.line_range,
-                                     self.min_instruction_length, self.maximum_operations_per_instruction)
-        #print(orig_size, len(data))
+                                     data, de, bool(self.default_is_stmt), self.opcode_base)
+        GenerateCommands(self.commands, de,
+                         bool(self.default_is_stmt), self.opcode_base)
+
+        # print(orig_size, len(data))
 
 
 if __name__ == "__main__":
@@ -318,9 +451,13 @@ if __name__ == "__main__":
         print(f"could not find line number info in {exe}")
         exit(1)
     # print(len(sec_line.data))
-    header = DebugLineHeader()
     data = io.BytesIO(sec_line.data)
+
+    units = []
     while data.tell() < len(sec_line.data):
         # print ("@@@@@@@@")
-        header.unpack(data)
+        lnp = LineNumberProgram()
+        lnp.unpack(data)
+        units.append(lnp)
+        break
     # print(header)
