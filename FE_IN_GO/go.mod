29f58dfa798c8b365ce8914a58715100487eb32f
csubrahmanyam <csubrahmanyam2@gmail.com>
2024-11-07 16:22:52 +0530

    parse cli args

--- FE_IN_GO/go.mod
+++ FE_IN_GO/go.mod
@@ -1,3 +1,5 @@
 module cwc
 
 go 1.23.3
+
+require github.com/akamensky/argparse v1.4.0 // indirect
--- FE_IN_GO/main.go
+++ FE_IN_GO/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/akamensky/argparse"
+)
+
+type Arguments struct {
+	shake_tree_flag *bool
+	stdlib_flag     *string
+	arch_flag       *string
+	emit_ir_flag    *bool
+	source_files    *string
+}
 
 func main() {
-    fmt.Println("go setup working!")
+	args := Arguments{}
+	parser := argparse.NewParser("pretty_printer", "pretty_printer")
+	args.shake_tree_flag = parser.Flag("", "shake_tree", &argparse.Options{Help: "remove unreachable functions"})
+	args.stdlib_flag = parser.String("", "stdlib", &argparse.Options{Help: "path to stdlib directory", Default: "./Lib"})
+	args.arch_flag = parser.String("", "arch", &argparse.Options{Help: "architecture to generated IR for", Default: "x64"})
+	args.emit_ir_flag = parser.Flag("", "emit_ir", &argparse.Options{Help: "stop at the given stage and emit ir"})
+	args.source_files = parser.String("F", "files", &argparse.Options{Required: true, Help: "an input source file"})
+
+	parse_err := parser.Parse(os.Args)
+	if parse_err != nil {
+		fmt.Print(parser.Usage(parse_err))
+		os.Exit(1)
+	}
+
+	//Debug Print
+	fmt.Println("shake tree flag:", *args.shake_tree_flag)
+	fmt.Println("stdlib flag:", *args.stdlib_flag)
+	fmt.Println("arch flag:", *args.arch_flag)
+	fmt.Println("emit_ir flag:", *args.emit_ir_flag)
+	fmt.Println("source files flag:", *args.source_files)
+	//Debug End
+
 }
