c732ac516a9141aecf046ffba253c778ce4c2db4
Robert Muth <robert@muth.org>
2025-02-04 17:29:03 -0500

    add lexer with peek()

--- FE/lexer.h
+++ FE/lexer.h
@@ -2,6 +2,9 @@
 // (c) Robert Muth - see LICENSE for more info
 #include <cstdint>
 #include <string_view>
+#include <vector>
+
+#include "Util/assert.h"
 
 namespace cwerg::fe {
 
@@ -70,6 +73,80 @@ class LexerRaw {
   const SrcLoc& GetSrcLoc() { return srcloc_; }
 
   TK_RAW Next();
+};
+
+struct TK {
+  TK_KIND kind = TK_KIND::INVALID;
+  std::string_view text;
+  SrcLoc sl;
+  std::vector<std::string_view> comments;
+  std::vector<std::string_view> annotations;
+};
+
+class Lexer {
+ private:
+  LexerRaw lexer_raw_;
+  TK peek_cached_;
+  TK current_;
+
+ public:
+  Lexer(std::string_view input, uint32_t file_id)
+      : lexer_raw_(input, file_id) {}
+
+  const TK& peek() {
+    if (peek_cached_.kind == TK_KIND::INVALID) {
+      peek_cached_ = next();
+    }
+    return peek_cached_;
+  }
+
+  bool match(TK_KIND kind, std::string_view text) {
+    peek();
+    if (kind == peek_cached_.kind &&
+        (text.empty() || text == peek_cached_.text)) {
+      peek_cached_.kind = TK_KIND::INVALID;
+      return true;
+    }
+    return false;
+  }
+
+  bool match_or_die(TK_KIND kind, std::string_view text) {
+    peek();
+    if (kind == peek_cached_.kind &&
+        (text.empty() || text == peek_cached_.text)) {
+      peek_cached_.kind = TK_KIND::INVALID;
+      return true;
+    }
+    ASSERT(false, "");
+    return false;
+  }
+
+  const TK& next() {
+    if (peek_cached_.kind != TK_KIND::INVALID) {
+      current_ = peek_cached_;
+      peek_cached_.kind = TK_KIND::INVALID;
+    } else {
+      current_.comments.clear();
+      TK_RAW tk = lexer_raw_.Next();
+      while (tk.kind == TK_KIND::COMMENT) {
+        current_.comments.push_back(tk.text);
+        tk = lexer_raw_.Next();
+      }
+      while (tk.kind == TK_KIND::ANNOTATION) {
+        current_.annotations.push_back(tk.text);
+        if (current_.annotations.empty()) {
+          current_.sl = lexer_raw_.GetSrcLoc();
+        }
+        tk = lexer_raw_.Next();
+      }
+      if (current_.annotations.empty()) {
+        current_.sl = lexer_raw_.GetSrcLoc();
+      }
+      current_.kind = tk.kind;
+      current_.text = tk.text;
+    }
+    return current_;
+  }
 };
 
 }  // namespace cwerg::fe
