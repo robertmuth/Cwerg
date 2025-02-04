cc63ef98ccd5ddd2a5ae6ffe6441d5ca1cdc8535
Robert Muth <robert@muth.org>
2025-02-03 21:26:44 -0500

    add missing benchmark source

---
+++ FE/lexer_bench.cc
@@ -0,0 +1,53 @@
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+#include "FE/lexer.h"
+#include "Util/switch.h"
+
+using namespace cwerg::fe;
+
+std::vector<char> SlurpDataFromStream(std::istream* fin) {
+  size_t num_bytes_per_read = 1024 * 1024;
+  size_t current_offset = 0U;
+  std::vector<char> out(num_bytes_per_read);
+  auto rdbuf = fin->rdbuf();
+  while (true) {
+    size_t count =
+        rdbuf->sgetn(out.data() + current_offset, num_bytes_per_read);
+    if (count == 0) break;
+    current_offset += count;
+    out.resize(current_offset + num_bytes_per_read);
+  }
+  out.resize(current_offset);
+  return out;
+}
+
+int main(int argc, const char* argv[]) {
+  bool verbose = true;
+  InitLexer();
+
+  // If the synchronization is turned off, the C++ standard streams are allowed
+  // to buffer their I/O independently from their stdio counterparts, which may
+  // be considerably faster in some cases.
+  std::ios_base::sync_with_stdio(false);
+
+  std::istream* fin = &std::cin;
+
+  std::vector<char> data = SlurpDataFromStream(fin);
+  LexerRaw lexer(
+      std::string_view(reinterpret_cast<char*>(data.data()), data.size()), 555);
+  // std::cout << "loaded " << data.size() << " bytes\n";
+
+  while (true) {
+    TK_RAW tk = lexer.Next();
+    if (tk.kind == TK_KIND::SPECIAL_EOF) break;
+    const SrcLoc& sl = lexer.GetSrcLoc();
+    if (verbose) {
+      std::cout << EnumToString(tk.kind) << " " << sl.line + 1 << " " << sl.col
+                << " " << tk.text << "\n";
+    }
+  }
+  return 0;
+}
