fad65dc1ef3aa60e036d14abfd5d36d0182de365
Robert Muth <robert@muth.org>
2025-04-17 11:36:16 -0400

    prepare c++ work on macros

---
+++ FE/macro.h
@@ -0,0 +1,10 @@
+#pragma once
+// (c) Robert Muth - see LICENSE for more info
+#include <map>
+
+#include "FE/cwast_gen.h"
+#include "Util/assert.h"
+
+namespace cwerg::fe {
+class MacroContext {};
+}  // namespace cwerg::fe
