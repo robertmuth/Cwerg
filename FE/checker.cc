cd2e5158a594f0c3785e3e257ea61cfe86287d57
Robert Muth <robert@muth.org>
2025-05-20 17:16:55 -0400

    add  checker.cc/.h placeholder files

---
+++ FE/checker.cc
@@ -0,0 +1,11 @@
+#include "FE/checker.h"
+
+#include <array>
+#include <map>
+#include <set>
+
+#include "Util/assert.h"
+
+namespace cwerg::fe {
+
+}  // namespace cwerg::fe
---
+++ FE/checker.h
@@ -0,0 +1,5 @@
+#pragma once
+// (c) Robert Muth - see LICENSE for more info
+
+namespace cwerg::fe {
+}  // namespace cwerg::fe
