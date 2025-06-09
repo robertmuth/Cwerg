87c295e530618d29b5ce27061934216914577634
Robert Muth <robert@muth.org>
2025-06-09 15:19:12 -0400

    prepare for eval implementation (c++)

---
+++ FE/eval.cc
@@ -0,0 +1,12 @@
+#include "FE/eval.h"
+
+#include <set>
+
+#include "FE/cwast_gen.h"
+#include "FE/symbolize.h"
+
+namespace cwerg::fe {
+namespace {
+}  // namespace
+
+}  // namespace cwerg::fe
---
+++ FE/eval.h
@@ -0,0 +1,11 @@
+#pragma once
+// (c) Robert Muth - see LICENSE for more info
+#include <map>
+
+#include "FE/cwast_gen.h"
+#include "FE/identifier.h"
+#include "Util/assert.h"
+
+namespace cwerg::fe {
+
+}  // namespace cwerg::fe
