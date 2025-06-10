62e47affb45f02e849e8b84b3183e2c9c4eda8f2
Robert Muth <robert@muth.org>
2025-06-10 15:26:38 -0400

    more work on eval (c++)

--- FE/eval.cc
+++ FE/eval.cc
@@ -4,9 +4,14 @@
 
 #include "FE/cwast_gen.h"
 #include "FE/symbolize.h"
+#include "Util/handle.h"
 
 namespace cwerg::fe {
 namespace {
 }  // namespace
 
+struct Stripe<ConstCore, Const> gConstCore("ConstCore");
+
+StripeBase* const gAllStripesConst[] = {&gConstCore, nullptr};
+struct StripeGroup gStripeGroupConst("Const", gAllStripesConst, 256 * 1024);
 }  // namespace cwerg::fe
--- FE/eval.h
+++ FE/eval.h
@@ -8,4 +8,162 @@
 
 namespace cwerg::fe {
 
+enum class CONST_KIND {
+  INVALID = 0,
+  //
+  S8 = 11,
+  S16 = 12,
+  S32 = 13,
+  S64 = 14,
+  //
+  U8 = 21,
+  U16 = 22,
+  U32 = 23,
+  U64 = 24,
+  //
+  R32 = 30,
+  R64 = 31,
+  //
+  BOOL = 41,
+  TYPEID = 42,
+  VOID = 43,
+  UNDEF = 44,
+  COMPLEX_DEFAULT = 45,
+  //
+  SYM_ADDR = 50,
+  FUN_ADDR = 51,
+  COMPOUND = 52,
+  //
+  SPAN = 60,
+};
+
+struct Const : public Handle {
+  explicit constexpr Const(uint32_t index = 0,
+                           CONST_KIND kind = CONST_KIND::INVALID)
+      : Handle(index, uint8_t(kind)) {}
+  explicit constexpr Const(Handle ref) : Handle(ref.value) {}
+};
+
+struct ConstCore {
+  union {
+    float val_r32;
+    double val_r64;
+    bool val_bool;
+    uint32_t val_u32;
+    uint64_t val_u64;
+    int32_t val_s32;
+    int64_t val_s64;
+    Node symbol;
+    struct {
+      Node pointers;
+      Const size;
+      Node content;
+    } span;
+  };
+};
+
+extern struct Stripe<ConstCore, Const> gConstCore;
+extern struct StripeGroup gStripeGroupConst;
+
+inline ConstCore& ConstGetCore(Const c) { return gConstCore[c]; }
+
+inline Const ConstNewLong(CONST_KIND kind) {
+  Const out = Const(gStripeGroupConst.New().index(), kind);
+  // ASSERT(out.index() != xxx, "");
+  return out;
+}
+
+inline bool ConstIsShort(Const num) { return int32_t(num.value) < 0; }
+inline bool ValIsShortConstUnsigned(uint32_t val) { return val < (1U << 23U); }
+
+inline bool ValIsShortConstSigned(uint64_t val) {
+  return -(1U << 12U) <= val && val < (1U << 22U);
+}
+
+inline Const ConstNewShortSigned(int64_t val, CONST_KIND kind) {
+  return Const(1U << 23U | (uint32_t)val, kind);
+}
+
+inline Const ConstNewShortUnsigned(uint32_t val, CONST_KIND kind) {
+  return Const(1U << 23U | val, kind);
+}
+
+inline uint32_t ConstShortGetUnsigned(Const c) { return c.value << 1U >> 9U; }
+inline int32_t ConstShortGetSigned(Const c) {
+  return int32_t(c.value) << 1U >> 9U;
+}
+
+inline Const ConstNewBool(bool val) {
+  return ConstNewShortUnsigned(val, CONST_KIND::BOOL);
+}
+
+inline Const ConstNewU8(uint8_t val) {
+  return ConstNewShortUnsigned(val, CONST_KIND::U8);
+}
+inline Const ConstNewU16(uint16_t val) {
+  return ConstNewShortUnsigned(val, CONST_KIND::U16);
+}
+inline Const ConstNewU32(uint32_t val) {
+  if (ValIsShortConstUnsigned(val)) {
+    return ConstNewShortUnsigned(val, CONST_KIND::U32);
+  }
+  Const out = ConstNewLong(CONST_KIND::U32);
+  ConstGetCore(out).val_u32 = val;
+  return out;
+}
+inline Const ConstNewU64(uint64_t val) {
+  if (ValIsShortConstUnsigned(val)) {
+    return ConstNewShortUnsigned(val, CONST_KIND::U64);
+  }
+  Const out = ConstNewLong(CONST_KIND::U64);
+  ConstGetCore(out).val_u64 = val;
+  return out;
+}
+
+inline Const ConstNewS8(int8_t val) {
+  return ConstNewShortSigned(val, CONST_KIND::S8);
+}
+inline Const ConstNewS16(int16_t val) {
+  return ConstNewShortSigned(val, CONST_KIND::S16);
+}
+inline Const ConstNewS32(int32_t val) {
+  if (ValIsShortConstSigned(val)) {
+    return ConstNewShortSigned(val, CONST_KIND::U32);
+  }
+  Const out = ConstNewLong(CONST_KIND::S32);
+  ConstGetCore(out).val_s32 = val;
+  return out;
+}
+inline Const ConstNewS64(int64_t val) {
+  if (ValIsShortConstSigned(val)) {
+    return ConstNewShortSigned(val, CONST_KIND::S64);
+  }
+  Const out = ConstNewLong(CONST_KIND::S64);
+  ConstGetCore(out).val_s64 = val;
+  return out;
+}
+inline Const ConstNewUndef() { return Const(0, CONST_KIND::UNDEF); }
+inline Const ConstNewVoid() { return Const(0, CONST_KIND::VOID); }
+inline Const ConstNewComplexDefault() {
+  return Const(0, CONST_KIND::COMPLEX_DEFAULT);
+}
+
+inline Const ConstNewSymAddr(Node sym) {
+  Const out = ConstNewLong(CONST_KIND::SYM_ADDR);
+  ConstGetCore(out).symbol = sym;
+  return out;
+}
+
+inline Const ConstNewFunAddr(Node sym) {
+  Const out = ConstNewLong(CONST_KIND::FUN_ADDR);
+  ConstGetCore(out).symbol = sym;
+  return out;
+}
+
+inline Const ConstNewCompound(Node sym) {
+  Const out = ConstNewLong(CONST_KIND::COMPOUND);
+  ConstGetCore(out).symbol = sym;
+  return out;
+}
+
 }  // namespace cwerg::fe
