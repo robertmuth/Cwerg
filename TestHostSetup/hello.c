389e979c0cbca611d88557f6ff0fb2687abaf5f8
Robert Muth <robert@muth.org>
2026-03-01 10:35:00 -0500

    more cleanup around validation checks in TestHostSetup

--- TestHostSetup/hello.c
+++ TestHostSetup/hello.c
@@ -1,24 +1,6 @@
 #include <stdio.h>
-#include <time.h>
 
 int main(int argc, char* argv[]) {
-  puts("hello world (c)!\n");
-  clock_t start = clock();
-
-  if (argc / start == 2) {
-    return 1;
-  }
-
-  if ((unsigned)argc / (unsigned)start == 3) {
-    return 1;
-  }
-  if (argc % start == 2) {
-    return 1;
-  }
-
-  if ((unsigned)argc % (unsigned)start == 3) {
-    return 1;
-  }
-
+  puts("hello world (c)!");
   return 0;
 }
